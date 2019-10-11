96cef9301946fdcf0373ada2be955ebbe05092c3
sanket tupe <tupesanket1999@gmail.com>
2019-10-12 03:03:54 +0530

    hi

---
+++ app/src/main/java/com/example/basketballcounterapp/login_screen.java
@@ -0,0 +1,104 @@
+package com.example.basketballcounterapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class login_screen extends AppCompatActivity {
+
+
+    public static FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        if(currentUser != null ){
+
+            updateUI(currentUser);
+
+            Intent intent = new Intent(login_screen.this, EnterTeamName.class);
+            startActivity(intent);
+            finish();
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+
+        Button login = findViewById(R.id.login);
+        TextView resister = findViewById(R.id.register);
+        login.setOnClickListener(clickListener);
+        resister.setOnClickListener(clickListener1);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+
+    }
+
+    public View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String pass;
+            final String user;
+            TextView emailId = findViewById(R.id.emailId);
+            TextView passWd = findViewById(R.id.passWd);
+
+            user = emailId.getText().toString();
+            pass = passWd.getText().toString();
+
+            if(pass.isEmpty() || user.isEmpty()){
+                Toast.makeText(getApplicationContext(),"Fields are Empty", Toast.LENGTH_SHORT).show();
+            }else{
+                firebaseAuth.signInWithEmailAndPassword(user, pass)
+                        .addOnCompleteListener(login_screen.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(getApplicationContext(),"Logged In  > "+ user +" successfully !", Toast.LENGTH_SHORT).show();
+
+                                    Intent intent = new Intent(login_screen.this,EnterTeamName.class);
+                                    startActivity(intent);
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(getApplicationContext(),"UN successfully !", Toast.LENGTH_SHORT).show();
+                                }
+
+                                // ...
+                            }
+                        });
+            }
+        }
+    };
+
+
+
+
+    public View.OnClickListener clickListener1 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Intent intent = new Intent(v.getContext(),register_screen.class);
+            startActivity(intent);
+            Toast.makeText(getApplicationContext(),"Resgister", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    public void sendData(){
+
+    }
+}
---
+++ app/src/main/java/com/example/basketballcounterapp/register_screen.java
@@ -0,0 +1,71 @@
+package com.example.basketballcounterapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class register_screen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+        Button button = findViewById(R.id.signup);
+        button.setOnClickListener(clickListener);
+    }
+
+
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            TextView emailcr = findViewById(R.id.emailIdcr);
+            TextView passcr = findViewById(R.id.passWdcr);
+            TextView name = findViewById(R.id.name);
+
+            final String email= emailcr.getText().toString();
+            String pass = passcr.getText().toString();
+            final String user_name = name.getText().toString();
+
+            if(pass.isEmpty() || email.isEmpty() || user_name.isEmpty()){
+                Toast.makeText(getApplicationContext(),"Fields are Empty", Toast.LENGTH_SHORT).show();
+            }else{
+
+                login_screen.firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener(register_screen.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                                    DatabaseReference myRef = firebaseDatabase.getReference("email");
+                                    myRef.setValue(email);
+
+                                    Toast.makeText(getApplicationContext(),"Created user  > "+email +" successfully !", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(register_screen.this, EnterTeamName.class);
+                                    startActivity(intent);
+
+                                } else {
+                                    Toast.makeText(getApplicationContext(),"Unsuccessfully !", Toast.LENGTH_SHORT).show();
+                                }
+                                // ...
+                            }
+                        });
+            }
+        }
+    };
+}
